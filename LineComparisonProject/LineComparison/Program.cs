cf83f3870b9eabffa9bb3de9e56631b2d7c99815
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 01:32:24 +0530

    [Aayush] added files for LineComparison TC1

---
+++ LineComparisonProject/LineComparison/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random R = new Random();
+            double x1 = R.Next(-100,100);
+            double y1 = R.Next(-100, 100);
+            double x2 = R.Next(-100, 100);
+            double y2 = R.Next(-100, 100);
+
+            double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            Console.WriteLine("length of the line is " + length);
+        }
+    }
+}
