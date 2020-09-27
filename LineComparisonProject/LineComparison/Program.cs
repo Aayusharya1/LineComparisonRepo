151afc1fba12bb4c857d9883c5c4938593d48be7
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 02:41:44 +0530

    [Aayush] added UC3 code

--- LineComparisonProject/LineComparison/Program.cs
+++ LineComparisonProject/LineComparison/Program.cs
@@ -7,13 +7,22 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Random R = new Random();
-            double x1 = R.Next(-100,100);
+            double x1 = R.Next(-100, 100);
             double y1 = R.Next(-100, 100);
             double x2 = R.Next(-100, 100);
             double y2 = R.Next(-100, 100);
 
-            double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-            Console.WriteLine("length of the line is " + length);
+            double x3 = R.Next(-100, 100);
+            double y3 = R.Next(-100, 100);
+            double x4 = R.Next(-100, 100);
+            double y4 = R.Next(-100, 100);
+
+            double length1 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            double length2 = Math.Sqrt(Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2));
+            if (length1 == length2) Console.WriteLine("length of the lines are equal ");
+            else if (length1 > length2) Console.WriteLine("length of the line1 is greater than line2 ");
+            else Console.WriteLine("length of the line1 is less than line2 ");
         }
+    }
     }
 }
