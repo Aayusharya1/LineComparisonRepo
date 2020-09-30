1cb19fd677ffdb9968d47a1123a062b29062f8d8
aayusharya1 <aayusharya07@gmail.com>
2020-09-30 19:26:12 +0530

    [Aayush] Add. Comparing using CompareTo

--- LineComparisonProject/LineComparison/Program.cs
+++ LineComparisonProject/LineComparison/Program.cs
@@ -18,11 +18,17 @@ namespace LineComparison
             double y4 = R.Next(-100, 100);
 
             double length1 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            Console.WriteLine("length of line1 = " + length1);
             double length2 = Math.Sqrt(Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2));
-            if (length1 == length2) Console.WriteLine("length of the lines are equal ");
-            else if (length1 > length2) Console.WriteLine("length of the line1 is greater than line2 ");
+            Console.WriteLine("length of line2 = " + length2);
+            if (length1.CompareTo(length2) == 0) Console.WriteLine("length of the lines are equal ");
+            else if (length1.CompareTo(length2) > 0) Console.WriteLine("length of the line1 is greater than line2 ");
             else Console.WriteLine("length of the line1 is less than line2 ");
         }
-    }
+
+
+
+
     }
 }
+
